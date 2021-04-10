2548c7b59feca50d944033effb7a6d27175d7330
ropunch04 <837329@lcps.org>
2021-04-10 17:59:48 -0400

    added winning functions + restart game
    future work: computer-generated selection (random at first, then implement minimax)

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+import time
 import pygame as pg
 import numpy as np
 
@@ -13,16 +14,14 @@ pg.init()
 pg.display.set_caption("Tic-Tac-Toe")
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-#Backend Board Creation
-board = np.zeros((3, 3))
-
 #Methods
-def create_board():
+def reset_board():
     screen.fill(BG_COLOR)
     pg.draw.line(screen, LINE_COLOR, (0, 200), (600, 200), 15)
     pg.draw.line(screen, LINE_COLOR, (0, 400), (600, 400), 15)
     pg.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), 15)
     pg.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), 15)
+    return np.zeros((3, 3))
 
 def draw_x(row, col):
     pg.draw.line(screen, X_COLOR, (25 + 200 * row, 25 + 200 * col), (175 + 200 * row, 175 + 200 * col), 15)
@@ -49,12 +48,52 @@ def filled():
                 return False
     return True
 
-create_board()
-current_player = 1
+def winner(player):
+    check = ''
+    board_flip = board
+    #horizontal check (transpose)
+    for col in range(3):
+        if board_flip[0][col] == player and board_flip[1][col] == player and board_flip[2][col] == player:
+            check, num = 'hor', col
+    #vertical check (transpose)
+    for row in range(3):
+        if board_flip[row][0] == player and board_flip[row][1] == player and board_flip[row][2] == player:
+            check, num = 'ver', row 
+    #diagnol check
+    if board_flip[0][0] == player and board_flip[1][1] == player and board_flip[2][2] == player:
+        check, num = 'asc', None
+    elif board_flip[0][2] == player and board_flip[1][1] == player and board_flip[2][0] == player:
+        check, num = 'desc', None
+    if check == '':
+        return False
+    else:
+        draw_winline(check, num, player)
+        return True
 
+def draw_winline(check, num, player):
+    colors = {
+        1 : X_COLOR,
+        2 : O_COLOR
+    }
+    win_color = colors.get(player)
+
+    if check == 'ver':
+        pg.draw.line(screen, win_color, (100 + 200 * num, 0), (100 + 200 * num, 600), 15)
+    elif check == 'hor':
+        pg.draw.line(screen, win_color, (0, 100 + 200 * num), (600, 100 + 200 * num), 15)
+    elif check == 'desc':
+        pg.draw.line(screen, win_color, (0, 600), (600, 0), 15)
+    elif check == 'asc':
+        pg.draw.line(screen, win_color, (0, 0), (600, 600), 15)
+
+#Main Body of Code
+board = reset_board()
+current_player = 1
 
 running = True
 while(running):
+    bool1 = False
+    bool2 = False
     for event in pg.event.get():
         if (event.type == pg.KEYDOWN):
             if (event.key == pg.K_q):
@@ -75,7 +114,13 @@ while(running):
                 elif current_player == 2:
                     mark_square(clicked_row, clicked_col, 2)
                     current_player = 1
+    bool1 = winner(1)
+    bool2 = winner(2)
     pg.display.flip()
+    if bool1 or bool2:
+        time.sleep(3)
+        board = reset_board()
+        current_player = 1
 pg.quit()
 
 
